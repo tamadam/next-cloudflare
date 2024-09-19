ebd327c3efc77b1382eb77b97bb15308483a0fc5
Adam <adam.tamas@ni.com>
2024-09-19 16:19:52 +0200

    Add client component

---
+++ app/components/Button.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+const Button = () => {
+    const [isActive, setActive] = useState(false)
+
+  return (
+    <button onClick={() => setActive(prev => !prev)} style={{backgroundColor: `${isActive ? 'red' : 'transparent'}`, padding: "1em"}}>Click me</button>
+  )
+}
+
+export default Button
--- app/content/page.tsx
+++ app/content/page.tsx
@@ -1,15 +1,22 @@
 // STATIC RENDERING - REVALIDATE AFTER X SEC (DEFINED BELOW)
 // ALSO CALLED Incremental Static Regeneration
+// ITS NOT SUPPORTED WHEN USING EDGE RUNTIME
+//
 
-export const runtime = 'edge'
+import Button from "../components/Button";
+
+
 
 export const revalidate = 10; // revalidate after 10 sec
+
+export const runtime = 'edge';
 
 const ContentPage = async () => {    
     const content = await fetch("https://whatthecommit.com/index.txt").then((response) => response.text());
 
   return (
-    <div style={{backgroundColor: "blanchedalmond"}}>{content}</div>
+    <div style={{backgroundColor: "blanchedalmond", display: "grid", placeItems: "center"}}>{content}
+    <Button /></div>
   )
 }
 
