334e66f1439e525214e96746f12da3f6905fc0f0
Adam <adam.tamas@ni.com>
2024-09-19 14:11:47 +0200

    Add fetch functionality

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,6 +1,25 @@
-const ProductsPage = () => {
+interface Product {
+  id: number;
+  title: string;
+}
+
+interface ProductsResponse {
+  products: Product[];
+}
+
+const ProductsPage = async () => {
+  const res = await fetch("https://dummyjson.com/products");
+  const data: ProductsResponse = await res.json();
+  const products: Product[] = data.products;
+  
   return (
-    <div>ProductsPage pull request</div>
+    <div>ProductsPage
+      <ul>
+        {products.map(product => (
+          <li key={product.id}>{product.title}</li>
+        ))}
+      </ul>
+    </div>
   )
 }
 
