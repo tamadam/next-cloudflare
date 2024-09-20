f142c04ce5552974e8b1527e6daa672f6675a142
Adam <adam.tamas@ni.com>
2024-09-20 12:11:25 +0200

    Add edge runtime

--- app/content/page.tsx
+++ app/content/page.tsx
@@ -5,6 +5,8 @@
 
 export const revalidate = 10;
 
+export const runtime = "edge";
+
 const ContentPage = async () => {
   const content = await fetch("https://whatthecommit.com/index.txt").then(
     (response) => response.text()
